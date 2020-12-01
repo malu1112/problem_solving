041f36529927289123bd03995bd84b721fc9446f
Malathi P J <44761851+malu1112@users.noreply.github.com>
2020-12-01 16:24:32 -0700

    Create paracheck.py

---
+++ paracheck.py
@@ -0,0 +1,24 @@
+
+def checkpara(s):
+    if len(s) % 2 != 0:
+        return False
+
+    opening = set('([{')
+    matches = set([('(', ')'), ('[', ']'), ('{', '}')])
+
+    stack = []
+
+    for paren in s:
+        if paren in opening:
+            stack.append(paren)
+        else:
+            if len(stack) == 0:
+                return False
+
+            last_open = stack.pop()
+
+            if (last_open, paren) not in matches:
+                return False
+    return len(stack) == 0
+
+print(checkpara('((([{})))'))
